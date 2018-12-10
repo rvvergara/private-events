e1f9bbba9dca01ca0a52f1e9bbbe6243d4c6aaf5
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-10 17:37:26 +0800

    Include an event model method for adding attendees
    1. Put it in event model because it's related to updating the events
    table in the database
    
    2. Call it add_event_attendee - it takes in a user as argument and it
    simply pushes that user into the attendees attribute of the event object

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,9 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :attendances, foreign_key: :event_id
 
-  has_many :attendees, through: :attendance
+  has_many :attendees, through: :attendances
+
+  def add_attendee(user)
+    self.attendees.push(user)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  name: "Michael Jordan",
+  username: "goat",
+  email: "jordan@gmail.com",
+  password:              "password",
+  password_confirmation: "password",
+)
+
+5.times do |n|
+User.create!(
+  name: "User-#{n}",
+  username: "user#{n}",
+  email: "user#{n}@gmail.com",
+  password:              "password",
+  password_confirmation: "password"
+)
+end
