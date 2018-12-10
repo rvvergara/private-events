c17a9ec8e1b7b689e70090f1233cd21b50391cd5
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-10 18:16:33 +0800

    Add scopes to event model
    1. Default scope of descending order of event creation
    2. Ascending order of event date to show first upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :attendances, foreign_key: :event_id
-
   has_many :attendees, through: :attendances
+  default_scope -> {order(created_at: :desc)}
+  scope :upcoming_events, -> {order(date: :desc)}
 
   def add_attendee(user)
     self.attendees.push(user)
