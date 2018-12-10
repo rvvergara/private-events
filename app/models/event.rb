e209737083321e70378824374fabbe34dfc3c71a
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-10 18:35:30 +0800

    Fix upcoming_events scope in event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
   has_many :attendances, foreign_key: :event_id
   has_many :attendees, through: :attendances
   default_scope -> {order(created_at: :desc)}
-  scope :upcoming_events, -> {order(date: :desc)}
+  scope :upcoming_events, -> {where("date < ?", Time.now.to_date + 20.days)}
 
   def add_attendee(user)
     self.attendees.push(user)
