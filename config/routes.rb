107f20075117d503727640a8cd92ba1e614ef9d5
dipto0321 <diptokmk47@gmail.com>
2018-12-10 14:16:42 +0600

    Fix log in problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,10 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'sessions/new'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/create'
-  get 'users/update'
-  get 'users/destroy'
 
-  
   get "signup", to: "users#new"
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
+  
   resources :users
 end
