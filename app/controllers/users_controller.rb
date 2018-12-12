8cfc48a3792a1fa7f511673142e03d35707934ab
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-12 11:51:05 +0800

    Log user in upon succesful account creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Your account has been created! Welcome"
+      flash[:success] = "Your account has been created! Welcome #{@user.name}!"
+      login(@user)
       redirect_to @user
     else
       render 'new'
