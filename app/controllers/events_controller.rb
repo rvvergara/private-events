7c0c154679d414af5aa5dcd441653ff53e5df077
dipto0321 <diptokmk47@gmail.com>
2018-12-12 09:30:37 +0600

    Add image feature

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -57,6 +57,6 @@ class EventsController < ApplicationController
   private
   
   def event_params
-    params.require(:event).permit(:title, :date, :venue, :description)
+    params.require(:event).permit(:title, :date, :venue, :description, :picture)
   end
 end
