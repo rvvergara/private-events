bee7dc0b75f795505fee4f6589cb48b5e4d1b63e
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-12 11:48:37 +0800

    Add flash messages to views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Your account has been created! Welcome"
       redirect_to @user
     else
       render 'new'
