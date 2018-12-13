20cda977af60753a5c991d6a5f28a0fd20d9b992
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-13 10:45:02 +0800

    Fix routing and associated action for joining event

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,15 +1,24 @@
 class AttendancesController < ApplicationController
-  
+  before_action :logged_in_user, only: :create
+
   def create
-    @event = Event.find_by(id: params[:id])
+    @user = current_user
+    @event = Event.find_by(id: params[:event_id])
+    @attendance = Attendance.new(attendance_params)
     begin
-      @event.add_attendee(current_user)
+      @attendance.save
     rescue => exception
       flash[:danger] = "You are already in this event"
       render "users/show"
     else
-      redirect_to @event
+      redirect_to event_path(@event)
     end
+  end
+
+  private
+
+  def attendance_params
+    params.require(:attendance).permit(:attendee_id, :event_id)
   end
 
 end
