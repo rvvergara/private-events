00bc1bbfabe9e068d3188a6e731eb19d14dc4d0a
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-19 10:44:44 +0800

    Add foreign key constraint to creator_id in events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_025854) do
+ActiveRecord::Schema.define(version: 2018_12_19_023223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,4 +46,5 @@ ActiveRecord::Schema.define(version: 2018_12_12_025854) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end
