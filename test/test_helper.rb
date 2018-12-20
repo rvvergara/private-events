93fa0662cd2c8ba870debf5f0a0ebe98bb451c64
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-20 10:29:01 +0800

    Remove test folder

--- test/test_helper.rb
+++
@@ -1,9 +0,0 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
-require 'minitest/reporters'
-Minitest::Reporters.use!
-
-class ActiveSupport::TestCase
-  fixtures :all
-end
