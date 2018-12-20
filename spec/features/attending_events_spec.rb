8435567c7d9cfaae0277bf914b7414ab3491031b
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-20 18:20:13 +0800

    Write spec for when a logged user attends event

--- spec/features/attending_events_spec.rb
+++ spec/features/attending_events_spec.rb
@@ -24,3 +24,31 @@ feature "non logged user tries to attend an upcoming event" do
   end
 
 end
+
+feature "logged user tries to attend upcoming event" do
+  before :each do
+    @user = User.first
+    @event = Event.last
+    page.set_rack_session(user_id: @user.id)
+    visit event_path(@event)
+    click_on("Attend Event")
+  end
+
+  scenario "user will now see the text 'You are attending this event' in lieu of Attend Event button" do
+    expect(page).to have_content("You are attending this event")
+  end
+
+  scenario "user will now see a link with his name in the list of attendees" do
+    within page.all("ul").last do
+      expect(page).to have_link(@user.name, href: user_path(@user))
+    end
+  end
+
+  scenario "user will now see this event in his page as part of his 'will attend' events" do
+    visit user_path(@user)
+    within page.all("ol").first do
+      expect(page).to have_link(@event.title, href: event_path(@event))
+    end
+  end
+
+end
