210516d4be55eb5f518350fa38ea0ff766e4a188
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-21 19:07:35 +0800

    Write specs for invitations show page

--- spec/features/users_invite_page_spec.rb
+++ spec/features/users_invite_page_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 feature "Users Invitation Page - perspective of event creator" do
-  
+  before :each do
+    @user = User.first
+    @event = FactoryBot.create(:future_event, creator_id: @user.id)
+    page.set_rack_session(user_id: @user.id)
+    visit event_path(@event)
+    click_on("Invite People")
+  end
+
+  scenario "user will be able to open the page" do
+    expect(page).to have_content("Invite Button Here")
+  end
+end
+
+feature "Perspective of visitor" do
+  before :each do
+    @creator = User.first
+    @visitor = User.last
+    @event = FactoryBot.create(:future_event, creator_id: @creator.id)
+    page.set_rack_session(user_id: @visitor.id)
+    visit invitations_path(@creator, @event)
+  end
+
+  scenario "visitor will be redirected to his own page" do
+    expect(page).to have_selector("h1", text: @visitor.name)
+  end
+
+  scenario "visitor will see a flash message" do
+    expect(page).to have_selector("div.alert.alert-danger", text: "You are not authorized to do that!")
+  end
 end
