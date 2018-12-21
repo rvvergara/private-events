61a6a5c16328b4c7b09e13c61252d577ea7de8c8
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-21 11:14:13 +0800

    Refactor sessions#destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,8 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_path
+    logout
   end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -40,6 +40,7 @@ module SessionsHelper
 
   def logout
     session[:user_id] = nil
+    redirect_to root_path
   end
 
 end
