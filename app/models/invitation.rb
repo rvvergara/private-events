b175e73c0ecb8900075fafa1af91000e8f10de45
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-22 09:18:52 +0800

    Add accept and decline to invitation
    1. Add those instance methods to Invitation model
    2. Update invitation spec to test those instance methods

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,14 @@
 class Invitation < ApplicationRecord
   belongs_to :event
   belongs_to :invitee, class_name: "User"
+
+  # Update accepted attribute to true
+  def accept
+    update(accepted: true)
+  end
+
+  # Update decline attribute to true
+  def decline
+    update(declined: true)
+  end
 end
--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,9 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Invitation do
+  
   describe "factories" do
     it "has a valid new_invitation factory" do
       expect(FactoryBot.build(:new_invitation)).to be_valid
     end
   end
+
+  describe "accept instance method" do
+    it "sets the invitation's accepted attribute to true" do
+      invitation = FactoryBot.create(:new_invitation)
+
+      invitation.accept
+
+      expect(invitation.accepted).to be(true)
+    end
+  end
+
+  describe "decline instance method" do
+    it "sets the invitation's declined attribute to true" do
+      invitation = FactoryBot.create(:new_invitation)
+
+      invitation.decline
+
+      expect(invitation.declined).to be(true)
+    end
+  end
+
 end
